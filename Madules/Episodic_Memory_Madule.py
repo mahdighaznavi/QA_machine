4f53091577bf7eb020eb0d42055121ddd42c4863
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2018-12-29 23:09:18 +0330

    episodic_memory_module function complete

--- Madules/Episodic_Memory_Madule.py
+++ Madules/Episodic_Memory_Madule.py
@@ -1,21 +1,47 @@
 import tensorflow as tf
 import numpy as np
+import hyper_parameters_and_constants as HP
 
-def create_gates(input_module_output, question, m, index, tensor, sentence_length, num_sentences=None):
-    if index == sentence_length:
+
+def create_gates(inputs, question, previous_time_step_output, index, tensor):
+    """
+
+    :param inputs: concat of embedding context sentences with shape [batch size, number of sentences, embedding size]
+    :param question: embedding of the question with shape [batch size, embedding size]
+    :param previous_time_step_output: output of previous time step with shape [batch size, embedding size]
+    :param index: index of cell
+    :param tensor: concat of Z's of previous cells
+    :return: a tensor with shape [batch size, sentence length]. g's.
+    """
+    if index == HP.num_sentence:
         return tf.nn.softmax(tensor)
-    a = np.array([index])
-    b = np.repeat(False, num_sentences)
-    b[a] = True
-    s = tf.boolean_mask(input_module_output, b)
-    z = tf.concat([s * question, s * m, tf.abs(tf.add(s, -1 * question)), tf.abs(tf.add(s, -1 * m))])
-    new_tensor = tf.concat(tensor, z)
-    return create_gates(input_module_output, question, m, index + 1, new_tensor)
+    b = np.repeat(False, HP.num_sentence)
+    b[index] = True
+    s = tf.boolean_mask(inputs, b, axis=0)
+    assert (tf.shape(s) != tf.shape(question))
+    z = tf.concat([s * question, s * previous_time_step_output, tf.abs(tf.add(s, -1 * question)),
+                   tf.abs(tf.add(s, -1 * previous_time_step_output))], 1)
+    dense_layer1 = tf.layers.Dense(HP.episodic_memory_dim, activation=tf.tanh)
+    dense_layer2 = tf.layers.Dense(2)
+    Z = dense_layer2(dense_layer1(z))
+    g = tf.nn.softmax(Z, axis=1)
+    new_tensor = tf.concat([tensor, g], 1)
+    return create_gates(inputs, question, previous_time_step_output, index + 1, new_tensor)
 
 
 def episodic_memory_module(input_module_output, question):
-    # TODO
-    g = create_gates(input_module_output, question, m, 0, )
-    def initializer():
-        temp = np.zeros()
-        return [False], tf.boolean_mask(input_module_output, np)
+    previous_time_step_output = tf.zeros(shape=[HP.batch_size, HP.embedding_size], dtype=tf.float32)
+    gru_cell = tf.nn.rnn_cell.GRUCell(HP.embedding_size)
+    for i in range(HP.episodic_memory_time_step):
+        gates = create_gates(input_module_output, question, previous_time_step_output, 0,
+                             tf.zeros(shape=[HP.batch_size, 0], dtype=tf.float32))
+        state = tf.zeros(shape=[HP.batch_size, HP.embedding_size], dtype=tf.float32)
+        for j in range(HP.num_sentence):
+            b = np.reshape(False, HP.num_sentence)
+            b[j] = True
+            g = tf.boolean_mask(gates, b)
+            input = tf.boolean_mask(input_module_output, b)
+            state = tf.add(tf.multiply(g, gru_cell(input, state)), tf.multiply((1 - g), state))
+
+        previous_time_step_output = state
+    return previous_time_step_output
--- hyper_parameters_and_constants.py
+++ hyper_parameters_and_constants.py
@@ -4,14 +4,13 @@ import numpy as np
 answer_module_hidden_size = 128
 max_sentence_length = 12
 embedding_size = 256
-sentence_length = 12
-num_sentences = 512
+num_sentence = 12
 episodic_memory_time_step = 2
 answer_module_time_step = 2
 batch_size = 64
-epsiodic_memory_dim = 128
+episodic_memory_dim = embedding_size
 word_numbers = 10000
 
 # constants
 EOS_token = 1
-mask = np.concatenate(np.repeat(False, sentence_length - 1), True)
+mask = np.concatenate(np.repeat(False, num_sentence - 1), True)
